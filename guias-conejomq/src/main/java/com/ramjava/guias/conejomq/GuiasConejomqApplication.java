f09493a0ace4a9e0a51198b352dff52f434eaba1
Ramjay <ramjayromorosa@yahoo.com>
2023-12-23 23:04:04 +0800

    confirmación inicial de guías-conejomq

---
+++ guias-conejomq/src/main/java/com/ramjava/guias/conejomq/GuiasConejomqApplication.java
@@ -0,0 +1,13 @@
+package com.ramjava.guias.conejomq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiasConejomqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiasConejomqApplication.class, args);
+	}
+
+}
---
+++ guias-conejomq/src/test/java/com/ramjava/guias/conejomq/GuiasConejomqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjava.guias.conejomq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiasConejomqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
